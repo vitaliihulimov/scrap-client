7b25feac131ffdaa0e049b0435721101a6034070
Vitalii Hulimov <vitaliygulimov@ukr.net>
2026-01-06 15:59:52 +0200

    fix vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
     plugins: [react()],
+
     server: {
         port: 5173,
         proxy: {
@@ -13,5 +15,14 @@ export default defineConfig({
                 rewrite: (path) => path.replace(/^\/api/, '/api')
             }
         }
-    }
-})
+    },
+
+    // ========================
+    // Для продакшн збірки на Render
+    // ========================
+    build: {
+        outDir: 'dist', // Vite буде збирати у client/dist
+        emptyOutDir: true,
+    },
+    base: './', // важливо для правильних шляхів до CSS/JS у Render
+});
